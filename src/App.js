e836f6abb59254e20e12552dde7164f548a3c57c
yllekjjirpvul98 <kyungchingman@gmail.com>
2019-11-06 22:36:14 +0000

    Initialize build

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import WrappedNormalLoginForm from './NormalLoginForm';
+import './App.css'
+
+function App() {
+  return (
+    <WrappedNormalLoginForm/>
+  );
+}
+
+export default App;
